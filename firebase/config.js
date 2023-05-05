a70dbb6ac8f344fa4fd64c5506333fc171bd2bc7
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-05-05 17:52:09 -0400

    add firebase auth

--- firebase/config.js
+++ firebase/config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -31,5 +32,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export {analytics, db}
+export {analytics, db, auth}
