7f8d1219666cb0440c55377283c6da0c7c1a55dc
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-05-05 17:32:33 -0400

    Create firebase app

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-firebase.initializeApp({
-  
-})
 
 export default function App() {
   return (
