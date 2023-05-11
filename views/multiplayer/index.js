f8d4395205652347b8a9c7a574ec8b9dfcc0997e
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-05-11 19:02:04 -0400

    Add singleplayer and multiplayer views

---
+++ views/multiplayer/index.js
@@ -0,0 +1,13 @@
+import { View, TouchableOpacity, Text, TextInput, Button } from 'react-native';
+
+function MultiPlayer() {
+
+  return (
+    <View>
+      <Text>Singleplayer mode</Text>
+    </View>
+  );
+
+}
+
+export default MultiPlayer;
---
+++ views/singleplayer/index.js
@@ -0,0 +1,13 @@
+import { View, TouchableOpacity, Text, TextInput, Button } from 'react-native';
+
+function SinglePlayer() {
+
+  return (
+    <View>
+      <Text>Singleplayer mode</Text>
+    </View>
+  );
+
+}
+
+export default SinglePlayer;
