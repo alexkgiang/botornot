6a5567f348376790bab7ff9eecc82104aadde2da
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-05-12 22:31:52 -0400

    [bf] Fix firebase messaging

--- views/singleGame/index.js
+++ views/singleGame/index.js
@@ -1,20 +1,20 @@
 import { SafeAreaView, View, TouchableOpacity, Text, TextInput, Button } from 'react-native';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { db } from '../../firebase/config';
-import { collection } from 'firebase/firestore';
+import { db, auth } from '../../firebase/config';
+import { collection, query, where, orderBy, limit } from 'firebase/firestore';
 
 function SingleGame() {
   const messageCollection = collection(db, 'messages');
-  // const query = messageCollection.orderBy('createdAt').limit(25);
+  const q = query(messageCollection, orderBy('createdAt'), limit(25));
 
-  // const [messages] = useCollectionData(query, {idField: 'id'});
+  const [messages] = useCollectionData(q, {idField: 'id'});
 
 
   return (
     <SafeAreaView>
       <Text>Game</Text>
       <View>
-        {/* {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)} */}
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
       </View>
     </SafeAreaView>
   );
@@ -22,7 +22,9 @@ function SingleGame() {
 }
 
 function ChatMessage(props) {
-    const { text, uid } = props.message;
+    const { text, uid, photoURL } = props.message;
+
+    const messageClass = uid === auth.currentUser.uid ? 'sentMessage' : 'receivedMessage';
 
     return (
       <Text>{text}</Text>
