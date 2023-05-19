d054e197c5ef358d437b4c95c8bdcc9a6e7a7d01
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-05-19 18:37:45 -0400

    [fe] add menu styling

--- views/menu/index.js
+++ views/menu/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { SafeAreaView, View, Text, Button } from "react-native"
+import { SafeAreaView, View, Text, Button, StyleSheet } from "react-native"
 import { auth } from "../../firebase/config";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-export default function Menu({ navigation }) {
+function Menu({ navigation }) {
   const signOut = async () => {
     try {
       await auth.signOut()
@@ -13,17 +13,72 @@ export default function Menu({ navigation }) {
   }
 
   return (
-    <SafeAreaView>
-      <Text>Menu</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('SinglePlayer')}>
-        <Text>singleplayer</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.topText}>Menu</Text>
+      <TouchableOpacity style={styles.singleplayer} onPress={() => navigation.navigate('SinglePlayer')}>
+        <Text style={styles.singleplayerText}>singleplayer</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('MultiPlayer')}>
-        <Text>multiplayer</Text>
+      <TouchableOpacity style={styles.multiplayer} onPress={() => navigation.navigate('MultiPlayer')}>
+        <Text style={styles.multiplayerText}>multiplayer</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={signOut}>
-        <Text>Sign Out</Text>
+      <TouchableOpacity style={styles.SignOutButton} onPress={signOut}>
+        <Text style={styles.SignOutText}>sign out</Text>
       </TouchableOpacity>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f8f8f8'
+  },
+  topText: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 30
+  },
+  singleplayer: {
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+    backgroundColor: '#4B7BEC',
+    marginBottom: 20,
+  },
+  singleplayerText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  multiplayer: {
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+    backgroundColor: '#34D399',
+    marginBottom: 20,
+  },
+  multiplayerText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  SignOutButton: {
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+    backgroundColor: '#F87171'
+  },
+  SignOutText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+});
+
+
+export default Menu;
