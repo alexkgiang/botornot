03e424eb54be13a7cf7007819ffe2e91bfa477b7
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-05-19 21:45:22 -0400

    Set up messaging styles

--- views/singleGame/index.js
+++ views/singleGame/index.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, TouchableOpacity, Text, TextInput, Button } from 'react-native';
+import { SafeAreaView, View, TouchableOpacity, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { db, auth } from '../../firebase/config';
 import { collection, query, where, orderBy, limit } from 'firebase/firestore';
@@ -18,17 +18,28 @@ function SingleGame() {
       </View>
     </SafeAreaView>
   );
-
 }
 
 function ChatMessage(props) {
     const { text, uid, photoURL } = props.message;
 
-    const messageClass = uid === auth.currentUser.uid ? 'sentMessage' : 'receivedMessage';
+    const messageClass = uid === auth.currentUser.uid;
 
     return (
-      <Text>{text}</Text>
+      <View messageStyle={messageStyles.messageContainer}>
+        <Image messageStyle={messageStyles.messageImage} source={photoURL} />
+        <Text messageStyle={messageClass ? messageStyles.sentMessage : messageStyles.receivedMessage}>{text}</Text>
+      </View>
     )
 }
+
+const messageStyles = StyleSheet.create({
+  messageContainer: {},
+  messageImage: {},
+  sentMessage: {
+    
+  },
+  receivedMessage: {},
+});
 
 export default SingleGame;
